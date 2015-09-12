a085a3c6f82a7faede6aaa8183fbe21ec09d8a56
Danya D. Smith <danyadsmith@gmail.com>
2015-09-12 14:57:04 -0400

    Add downvote feature

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,12 @@
 class Topic < ActiveRecord::Base
   has_many :votes, dependent: :destroy
+
+  def upvote
+    votes.create
+  end
+
+  def downvote
+    votes.last.destroy unless votes.empty?
+  end
+
 end
